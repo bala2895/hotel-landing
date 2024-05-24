a383fe9ff220e728e0e466f84c4d1e13cda60289
bala2895 <bala280195@gmail.com>
2024-05-24 08:56:19 +0530

    first commit

---
+++ hotel.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add code here to handle form submission, like sending data to a server
+    alert('Form submitted!');
+});
